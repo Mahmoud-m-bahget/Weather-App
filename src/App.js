ad11699316cc6c95ea3f9785351c0842cd77359f
mahmoud mostafa <mahmoodbebo.mb@gmail.com>
2020-09-21 23:52:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
+import Form from './Component/Form';
+import Weather from './Component/Weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// http://api.openweathermap.org/data/2.5/weather?q=cairo,egypt&appid=e36ed364400282e43250b6c4c0274d44
+const API_KEY = '929044d104fafae10a60a71b30c7bc00'
+
+class App extends Component{
+  state = {
+    tempreatuser :'',
+    city :'',
+    country :'',
+    humidity :'',
+    description :'',
+    error :'',
+  }
+
+  getWeather = async (e) =>{
+    e.preventDefault()
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api.json();
+    if (city&&country){
+      this.setState({
+        tempreatuser :data.main.temp,
+        city :data.name,
+        country :data.sys.country,
+        humidity :data.main.humidity,
+        description :data.weather[0].description,
+        error :'',
+      })
+    }else{
+      this.setState({
+        tempreatuser :'',
+        city :'',
+        country :'',
+        humidity :'',
+        description :'',
+        error :'Please Enter Data',
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="form-container">
+          <Form getWeather={this.getWeather}/>
+          <Weather
+            tempreatuser ={this.state.tempreatuser}
+            city ={this.state.city}
+            country ={this.state.country}
+            humidity ={this.state.humidity}
+            description ={this.state.description}
+            error ={this.state.error}
+            />
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Component/Weather.js
@@ -0,0 +1,47 @@
+import React  from 'react';
+
+const Weather =(props)=> {
+
+        return(
+            <div className="info">
+                {
+                    props.tempreatuser &&
+                    <p className = "info_key">Tempreatuser:  
+                        <span className="info_value"> {props.tempreatuser}</span>
+                    </p>
+                }
+                {
+                    props.city &&
+                    <p className = "info_key">City: 
+                        <span className="info_value"> {props.city}</span>
+                    </p>
+                }
+                {
+                    props.country &&
+                    <p className = "info_key">Country: 
+                        <span className="info_value"> {props.country}</span>
+                    </p>
+                }
+                {
+                    props.humidity &&
+                    <p className = "info_key"> Humidity: 
+                        <span className="info_value"> {props.humidity}</span>
+                    </p>
+                }
+                {
+                    props.description &&
+                    <p className = "info_key"> Description:
+                        <span className="info_value"> {props.description}</span>
+                    </p>
+                }
+                {
+                    props.error &&
+                    <p className = "info_key"> Error: 
+                       <span className="info_value"> {props.error}</span>
+                    </p>
+                }
+            </div>
+        )
+    
+};
+export default Weather;
